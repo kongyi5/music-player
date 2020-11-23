9a051e14309003591b409641902b211fb160e666
kongyi <jerrykongyi1997@gmail.com>
2020-11-23 09:56:57 +0800

    music play

--- dist/js.d818e0ef.js
+++ dist/js.d818e0ef.js
@@ -130,7 +130,66 @@ svgPlaceHolder.innerHTML = "\n<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<svg s
 
 require("./icons.js");
 
-console.log('hi lk');
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var $ = function $(selector) {
+  return document.querySelector(selector);
+};
+
+var $$ = function $$(selector) {
+  return document.querySelectorAll(selector);
+};
+
+var Player = /*#__PURE__*/function () {
+  function Player(node) {
+    _classCallCheck(this, Player);
+
+    this.root = typeof node === 'string' ? $(node) : node;
+    this.audio = new Audio();
+    this.songList = [];
+    this.currentIndex = 0;
+    this.start();
+    this.bind();
+  }
+
+  _createClass(Player, [{
+    key: "start",
+    value: function start() {
+      var _this = this;
+
+      fetch('https://jirengu.github.io/data-mock/huawei-music/music-list.json').then(function (res) {
+        return res.json();
+      }).then(function (data) {
+        console.log(data);
+        _this.songList = data;
+      });
+    }
+  }, {
+    key: "bind",
+    value: function bind() {
+      var self = this;
+
+      this.root.querySelector('.btn-play-pause').onclick = function () {
+        console.log(self.audio);
+        self.playSong();
+      };
+    }
+  }, {
+    key: "playSong",
+    value: function playSong() {
+      this.audio.src = this.songList[this.currentIndex].url;
+      this.audio.play();
+    }
+  }]);
+
+  return Player;
+}();
+
+new Player('#player');
 },{"./icons.js":"src/js/icons.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -159,7 +218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61211" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56701" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,36 @@
-console.log('hi lk')
 import './icons.js'
+
+const $ = selector => document.querySelector(selector)
+const $$ = selector => document.querySelectorAll(selector)
+
+class Player{
+  constructor(node){
+    this.root = typeof node === 'string' ? $(node) : node
+    this.audio = new Audio()
+    this.songList = []
+    this.currentIndex = 0
+    this.start()
+    this.bind()
+  }
+  start(){
+    fetch('https://jirengu.github.io/data-mock/huawei-music/music-list.json')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.songList = data
+      })
+  }
+  bind(){
+    const self = this
+    this.root.querySelector('.btn-play-pause').onclick = function(){
+      console.log(self.audio)
+      self.playSong()
+    }
+  }
+  playSong(){
+    this.audio.src = this.songList[this.currentIndex].url
+    this.audio.play()
+  }
+}
+
+new Player('#player')
